8891a1f6d759ab85a04243c4a768710461fa6e87
Dviela <a19230@svalero.com>
2022-05-05 18:42:44 +0200

    Borrado de primera linea y modificación en prueba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[]args){
-        System.out.print("Hello There");
-        System.out.print("Prueba 2");
+        System.out.print("Prueba 1");
     }
 }
 
